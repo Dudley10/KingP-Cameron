67a3f188e7941eabc103da1ade50505fd35279d7
Dudley10 <cameron.dudley@bruins.belmont.edu>
2025-01-24 23:03:39 -0600

    Tried doing launch
    the code doesn't like the methods and just redid the UI because lost it some how

--- Assets/Script/Ball Behavior.cs
+++ Assets/Script/Ball Behavior.cs
@@ -14,7 +14,11 @@ public class BallBehavior : MonoBehaviour
     public float minTimeToLaunch;
     public float maxTimeToLaunch;
     public float cooldown;
-    //public bool 
+    public float timeSinceLastLaunch;
+    public bool launching;
+    public float launchDuration;
+    public float timeLastLaunch;
+    public float timeLaunchDuration;
     public Vector2 targetPosition;
 
     public int secondsToMaxSpeed;
@@ -25,36 +29,120 @@ public class BallBehavior : MonoBehaviour
     //secondsToMaxSpeed = 30.0f;
     //minSpeed = 0.75f;
     //maxSpeed = 2.0f;
-     targetPosition = getRandomPosition();   
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        Vector2 currentPosition = gameObject.GetComponent<Transform>().position;
-        float distance = Vector2.Distance(currentPosition, targetPosition);
-        if (distance > 0.1f) {
-        //if (targetPosition != currentPosition) {
-            float difficulty = getDifficultyPercentage();
-            float currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
-            currentSpeed = currentSpeed * Time.deltaTime;
-            Vector2 newPosition = Vector2.MoveTowards(currentPosition, targetPosition, currentSpeed);
-            transform.position = newPosition;
-        } else {
-            targetPosition = getRandomPosition();
-        }
-        getRandomPosition();
-    
-        }
-        Vector2 getRandomPosition(){
+     targetPosition = getRandomPosition();  
+    Vector2 getRandomPosition(){
             float randomX = Random.Range(minX, maxX);
             float randomY = Random.Range(minY, maxY);
             Vector2 v = new Vector2(randomX, randomY);
             return v;
         }
-        public float getDifficultyPercentage() {
+        //public float
+        private float getDifficultyPercentage() {
         float difficulty=Mathf.Clamp01 (Time.timeSinceLevelLoad / secondsToMaxSpeed);
         return difficulty;
     }
+    
+    private void launch() {
+        targetPosition = target.transform.position;
+        if (launching == false) {
+            timeLaunchStart = Time.time;
+            launching = true;
+        }
+    }
+    //public bool
+    private bool onCooldown() {
+        bool onCooldown = true;
+        float timeSinceLastLaunch = (Time.time - timeLastLaunch);
+        if(timeSinceLastLaunch > cooldown) {
+           onCooldown = false;
+        }
+        return onCooldown;
+    }
+    //public void
+    private void startCooldown() {
+        launching = false;
+        timeLastLaunch = Time.time;
+    }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPosition = gameObject.GetComponent<Transform>().position;
+        if (onCooldown() == false) {
+            if (launching == true) {
+                float currentLaunchTime = Time.time - timeLaunchStart;
+                if (currentLaunchTime > launchDuration) {
+                     startCooldown();
+                }
+            } else {
+                launch();
+            }
+        
+        Vector2 currentPosition = transform.position;
+        float distance = Vector2.Distance((Vector2) transform.position, targetPosition);
+        if (distance > 0.1f) {
+            float currentSpeed;
+            if(launching == true) {
+                currentSpeed = Mathf.Lerp(minLaunchSpeed, maxLaunchSpeed, getDifficultyPercentage());
+            } else{
+                currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, getDifficultyPercentage());
+            }
+            currentSpeed = currentSpeed * Time.deltaTime;
+            Vector2 newPosition = Vector2.MoveTowards(currentPosition, targetPosition, currentSpeed);
+            transform.position = newPosition;
+        } else {
+            if(launching == true) {
+                startCooldown();
+            }
+            targetPosition = randomPosition();
+        }
+        }
+        // }
+        // float timeLaunching = Time.time - timeLastLaunch;
+        // if(timeLaunching > launchDuration){
+        //     startCooldown();
+        // }
+        // else {
+        //     launch();
+        // }
+        // getRandomPosition();
+        //}
+        //}
+        //}
+    //     Vector2 getRandomPosition(){
+    //         float randomX = Random.Range(minX, maxX);
+    //         float randomY = Random.Range(minY, maxY);
+    //         Vector2 v = new Vector2(randomX, randomY);
+    //         return v;
+    //     }
+    //     //public float
+    //     public float getDifficultyPercentage() {
+    //     float difficulty=Mathf.Clamp01 (Time.timeSinceLevelLoad / secondsToMaxSpeed);
+    //     return difficulty;
+    // }
+    
+    // public void launch() {
+    //     targetPosition = target.transform.position;
+    //     if (launching == false) {
+    //         timeLaunchStart = Time.time;
+    //         launching = true;
+    //     }
+    // }
+    // //public bool
+    // public bool onCooldown() {
+    //     bool onCooldown = true;
+    //     float timeSinceLastLaunch = (Time.time - timeLastLaunch);
+    //     if(timeSinceLastLaunch > cooldown) {
+    //        onCooldown = false;
+    //     }
+    //     return onCooldown;
+    // }
+    // //public void
+    // public void startCooldown() {
+    //     launching = false;
+    //     timeLastLaunch = Time.time;
+    // }
 }
+}
+
