895ccbed7e09cd29bee2ac93fc2a1d319c41cd15
Dudley10 <cameron.dudley@bruins.belmont.edu>
2025-02-07 10:49:56 -0600

    fixed the pin
    the pin no longer vibrates because of the transform method

--- Assets/Script/Pin Behavior.cs
+++ Assets/Script/Pin Behavior.cs
@@ -6,18 +6,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public Vector2 newPosition;
     public Vector3 mousePosG;
     Camera cam;
+    Rigidbody2D body;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = Camera.main;
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
      mousePosG = cam.ScreenToWorldPoint(Input.mousePosition);
-     newPosition = Vector2.MoveTowards(transform.position, mousePosG, speed * Time.fixedDeltaTime);
-     transform.position = newPosition;   
+     newPosition = Vector2.MoveTowards(body.position, mousePosG, speed * Time.fixedDeltaTime);
+     body.position = newPosition;   
     }
     private void OnCollisionEnter2D(Collision2D collision) {
         string collided = collision.gameObject.tag;
