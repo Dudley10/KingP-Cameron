6dda80d50d2c48dc6f0b6122661a70be4b0a595d
Dudley10 <cameron.dudley@bruins.belmont.edu>
2025-02-10 15:27:45 -0600

    Added better dash UI
    the dash icon now has a gradient on top when it's getting refreshed

--- Assets/Script/DashIconBehavior.cs
+++ Assets/Script/DashIconBehavior.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class DashIconBehavior : MonoBehaviour
 { TextMeshProUGUI label;
+Image overlay;
 float cooldown;
 float cooldownRate;
 
@@ -9,7 +11,14 @@ float cooldownRate;
     void Start()
     {
         label = GetComponentInChildren<TextMeshProUGUI>();
+        Image[] images = GetComponentsInChildren<Image>();
+        for (int i=0; i < images.Length; i++) {
+            if(images[i].tag == "overlay") {
+                overlay = images [i];
+            }
+        }
         cooldownRate = NewMonoBehaviourScript.cooldownRate;
+        overlay.fillAmount = 0.0f;
     }
 
     // Update is called once per frame
@@ -18,7 +27,9 @@ float cooldownRate;
         cooldown = NewMonoBehaviourScript.cooldown;
         string message = "";
         if (cooldown > 0.0) {
+            float fill = cooldown / cooldownRate;
             message = string.Format("{0:0.0}", cooldown);
+            overlay.fillAmount = fill;
         }
         label.SetText(message);
     }
